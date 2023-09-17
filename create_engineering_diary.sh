9a1fea3b83dcdcdd0b712bf7a34580c6eb77b0e4
ko <kou@ren-ga.jp>
2023-09-17 16:27:03 +0900

    add: 曜日も表示するように調整

--- create_engineering_diary.sh
+++ create_engineering_diary.sh
@@ -5,7 +5,9 @@ rm ./engineering-diary.md
 for i in `seq 0 6`
 do
     date=`date -v +${i}d '+%Y-%m-%d'`
-    echo "# $date" >> "./engineering-diary.md"
+    day_of_week=`date -v +${i}d '+%A'`
+
+    echo "# $date ($day_of_week)" >> "./engineering-diary.md"
     echo "" >> "./engineering-diary.md"
     echo "" >> "./engineering-diary.md"
     echo "" >> "./engineering-diary.md"
